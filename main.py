def2c4ceeeede0c3977941d6f242f91ec5a86621
IvanProgramming <vlasov.ivan.2004@gmail.com>
2020-10-19 00:50:37 +0300

    Ping added to list of commands

---
+++ main.py
@@ -0,0 +1,12 @@
+import discord
+
+import utils
+import settings
+
+client = discord.Client()
+@client.event
+async def on_message(message):
+    if message.author.bot == False and message.content == "ping":
+        await utils.ping(message)
+
+client.run(settings.DISCORD_TOKEN, bot=settings.IS_BOT)
